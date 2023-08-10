c59efc33bb43930c8b95618aefe95e1da196b4de
ali829 <alielyassraoui@gmail.com>
2023-08-11 01:02:46 +0200

    create child classes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 class Media {
-  constructor(title, ratings) {
+  constructor(title) {
     this._title = title;
     this._isCheckedOut = false;
-    this._ratings = ratings;
+    this._ratings = [];
   }
 
   //   setters
@@ -10,7 +10,7 @@ class Media {
     return this._title;
   }
   get isCheckedOut() {
-    return this._title;
+    return this._isCheckedOut;
   }
   get ratings() {
     return this._ratings;
@@ -27,12 +27,69 @@ class Media {
    * this method calculate the average rating
    */
   getAverageRating() {
-    return (
+    let avgRate =
       this._ratings.reduce((previousV, currentV) => previousV + currentV) /
-      this._ratings.length
-    );
+      this._ratings.length;
+    return Math.floor(avgRate);
   }
   addRating(rateValue) {
     this._ratings.push(rateValue);
   }
 }
+
+// book child class of media
+class Book extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  // getters
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+// Movie child class of media
+class Movie extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  // getters
+  get director() {
+    return this._director;
+  }
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+// book instance
+const historyOfEverything = new Book(
+  "A Short History of Nearly Everything",
+  "Bill Bryson",
+  544
+);
+historyOfEverything.toggleCheckOutStatus();
+console.log("value of checkout :", historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(
+  "Average rating of the book :",
+  historyOfEverything.getAverageRating()
+);
+
+// Movie instance
+const speed = new Movie("Speed", "Jan de Bont", 116);
+speed.toggleCheckOutStatus();
+console.log("speed movie stats:", speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log('speed average rating :',speed.getAverageRating());
